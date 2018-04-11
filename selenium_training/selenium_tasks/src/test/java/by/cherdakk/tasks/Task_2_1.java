f7abe3dc5d8c6cd59ff16755e335a7323c9c4ff4
anton dutko <capitanutka@gmail.com>
2018-04-11 22:58:52 +0300

    Project has been created

---
+++ selenium_training/selenium_tasks/src/test/java/by/cherdakk/tasks/Task_2_1.java
@@ -0,0 +1,28 @@
+package by.cherdakk.tasks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task_2_1 {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void task_2_1() {
+        driver.get("http://www.google.com/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
