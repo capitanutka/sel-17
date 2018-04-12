151d750f6f6f2d5f96a455c9100ed15a2b252967
anton dutko <capitanutka@gmail.com>
2018-04-12 19:03:08 +0300

    Task_3 added

---
+++ selenium_training/selenium_tasks/src/test/java/by/cherdakk/tasks/Task_3.java
@@ -0,0 +1,37 @@
+package by.cherdakk.tasks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Task_3 {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void task_3() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#notices .success"))));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
