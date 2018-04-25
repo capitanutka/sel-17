4fdf99c8730d96e28dd1d17e4ceac2788889b4a9
anton dutko <capitanutka@gmail.com>
2018-04-25 16:50:49 +0300

    Task9 is added

---
+++ selenium_training/selenium_tasks/src/test/java/by/cherdakk/tasks/Task_9.java
@@ -0,0 +1,84 @@
+package by.cherdakk.tasks;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Task_9 {
+  private WebDriver driver;
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    driver.get("http://localhost/litecart/admin/");
+    driver.findElement(By.cssSelector("[name=username]")).sendKeys("admin");
+    driver.findElement(By.cssSelector("[name=password]")).sendKeys("admin");
+    driver.findElement(By.cssSelector("[name=login]")).click();
+  }
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+
+  @Test
+  public void test_1A_countriesAreSorted() {
+    List<String> countries = new ArrayList<>();
+    driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
+    List<WebElement> nameCells = driver.findElements(By.cssSelector("form[name=countries_form] tr.row td:nth-child(5) a"));
+
+    for (WebElement nameCell : nameCells) {
+      countries.add(nameCell.getText());
+    }
+
+    List<String> countriesSorted = new ArrayList<>(countries);
+    countriesSorted.sort(Comparator.naturalOrder());
+    Assert.assertEquals(countriesSorted, countries);
+  }
+
+  @Test
+  public void test_1B_zonesAreSorted() {
+    List<String> failedItems = new ArrayList<>();
+
+    List<String> links = new ArrayList<>();
+    driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
+    List<WebElement> countryNameCells = driver.findElements(By.xpath("//form//tr[@class='row']/td[6][not(.='0')]/../td[5]/a"));
+    for (WebElement countryNameCell : countryNameCells) {
+      links.add(countryNameCell.getAttribute("href"));
+    }
+
+    for (String link : links) {
+      List<String> zones = new ArrayList<>();
+      driver.get(link);
+      List<WebElement> zoneNameCells = driver.findElements(By.cssSelector("table#table-zones td:nth-child(3)"));
+      for (int i = 0; i < zoneNameCells.size() - 1; i++) {
+        zones.add(zoneNameCells.get(i).getText());
+      }
+
+      List<String> zonesSorted = new ArrayList<>(zones);
+      zonesSorted.sort(Comparator.naturalOrder());
+
+      if (!zones.equals(zonesSorted)) {
+        failedItems.add(link);
+      }
+    }
+
+    Assert.assertTrue("Links to countries with unsorted zones: " + failedItems.toString(), failedItems.size() == 0);
+
+  }
+
+}
